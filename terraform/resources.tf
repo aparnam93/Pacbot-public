1d4833adc9b9006d442daf7007832839514e0bcc
aparnam93 <38241459+aparnam93@users.noreply.github.com>
2019-10-23 13:49:29 +0530

    Create resources.tf

---
+++ terraform/resources.tf
@@ -0,0 +1,88 @@
+resource "aws_instance" "pacbot-server" {
+   ami = "${var.ami}"
+   instance_type = "t2.large"
+   key_name = "${var.ami}"
+   subnet_id = "${var.subnetId}"
+   vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
+   iam_instance_profile = "${aws_iam_instance_profile.pacbot_instance_profile.name}"
+   source_dest_check = false
+   user_data = "${file("userdata.sh")}"
+   tags {
+     Name = "pacbot-server"
+     Application = "pacbot-server"
+     Environment = "Dev"
+     SysName = "Pacbot"
+     Scheduler = ""
+     Backup = "Backup"
+     Monitoring = "Null"
+     SSMManged = "False"
+     Patch Group = "False"
+     TSM = "aparna.manjunath@regeneron.com"
+     SysOwner = "aparna.manjunath@regeneron.com"
+     CostCenter = "0512"
+  }
+}
+
+resource "aws_eip" "lb" {
+  instance = "${aws_instance.server.id}"
+  vpc      = true
+}
+
+resource "aws_iam_role" "pacbot_server_role" {
+  name = "pacbot_server_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "pacbot_instance_profile" {
+  name = "pacbot_instance_profile"
+  role = "${aws_iam_role.pacbot_server_role.name}"
+}
+resource "aws_iam_role_policy" "pacbot_server_policy" {
+  name = "pacbot_server_policy"
+  role = "${aws_iam_role.pacbot_server_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+		  "rds:*",
+		  "s3:*",
+		  "redshift:*",
+		  "logs:*",
+		  "elasticloadbalancing:*",
+		  "iam:*",
+		  "es:*",
+		  "batch:*",
+		  "cloudwatch:*",
+		  "lambda:*",
+		  "ecs:*",
+		  "ec2:*",
+		  "ecr:*",
+		  "events:*"
+	    ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
