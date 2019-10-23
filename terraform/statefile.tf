05e3af1af97158cce8b4d56d95dc1269ef2eed22
aparnam93 <38241459+aparnam93@users.noreply.github.com>
2019-10-23 12:29:32 +0530

    Delete statefile.tf

--- terraform/statefile.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-  # Replace this with your bucket name!
-    bucket         = "${var.s3BucketName}"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
