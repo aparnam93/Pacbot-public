0b9a7c0347e0149fe4fcfd32c893a147e7f88e07
aparnam93 <38241459+aparnam93@users.noreply.github.com>
2019-10-23 13:51:43 +0530

    Update resources.tf

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "pacbot-server" {
    key_name = "${var.ami}"
    subnet_id = "${var.subnetId}"
    vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
-   iam_instance_profile = "${aws_iam_instance_profile.pacbot_instance_profile.name}"
    source_dest_check = false
    user_data = "${file("userdata.sh")}"
    tags {
@@ -28,61 +27,5 @@ resource "aws_eip" "lb" {
   vpc      = true
 }
 
-resource "aws_iam_role" "pacbot_server_role" {
-  name = "pacbot_server_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_instance_profile" "pacbot_instance_profile" {
-  name = "pacbot_instance_profile"
-  role = "${aws_iam_role.pacbot_server_role.name}"
-}
-resource "aws_iam_role_policy" "pacbot_server_policy" {
-  name = "pacbot_server_policy"
-  role = "${aws_iam_role.pacbot_server_role.id}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-		  "rds:*",
-		  "s3:*",
-		  "redshift:*",
-		  "logs:*",
-		  "elasticloadbalancing:*",
-		  "iam:*",
-		  "es:*",
-		  "batch:*",
-		  "cloudwatch:*",
-		  "lambda:*",
-		  "ecs:*",
-		  "ec2:*",
-		  "ecr:*",
-		  "events:*"
-	    ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
 
 
