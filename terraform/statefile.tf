4d3e5eeaa89e68be6e9bfaa125842d2b7b7f3147
aparnam93 <38241459+aparnam93@users.noreply.github.com>
2019-10-23 12:19:04 +0530

    Update statefile.tf

--- terraform/statefile.tf
+++ terraform/statefile.tf
@@ -4,9 +4,5 @@ terraform {
     bucket         = "${var.s3BucketName}"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-	
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "${var.StateLockDbName}"
-    encrypt        = true
   }
 }
