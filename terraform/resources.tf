482aa2530914cc71a47b45362c5ed9f3fdfadb37
aparnam93 <38241459+aparnam93@users.noreply.github.com>
2019-10-23 14:17:29 +0530

    Update resources.tf

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -6,7 +6,6 @@ resource "aws_instance" "pacbot_server" {
    vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
    user_data = "${file("userdata.sh")}"
 }
-
 resource "aws_iam_role" "pacbot_server_role" {
   name = "pacbot_server_role"
 
