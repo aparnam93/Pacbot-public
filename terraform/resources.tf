d8185ae65fca2973ae301184de4c317356e639a6
aparnam93 <38241459+aparnam93@users.noreply.github.com>
2019-10-23 14:07:35 +0530

    Update resources.tf

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "pacbot-server" {
+resource "aws_instance" "pacbot_server" {
    ami = "${var.ami}"
    instance_type = "t2.large"
    key_name = "${var.key-pair}"
-   subnet_id = "${aws_subnet.subnetId.id}"
+   subnet_id = "${var.subnetId}"
    vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
    user_data = "${file("userdata.sh")}"
 }
 
 
 resource "aws_eip" "lb" {
-  instance = "${aws_instance.pacbot-server.id}"
+  instance = "${aws_instance.pacbot_server.id}"
   vpc      = true
 }
