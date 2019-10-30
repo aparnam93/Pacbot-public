7577f5d4d63375d7f074549f02870113aed5ded2
aparnam93 <priyanka.sharma@regeneron.com>
2019-10-30 20:30:38 +0530

    adding a shell script

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -18,23 +18,4 @@ module "pacbot_sec_grp" {
 
 module "pacbot_installer_Server" {
   source = "modules/ec2_instance"
-  SysName_tag = "${var.SysName_tag}"
-  instance_type = "${var.instance_type}"
-  linux_base_ami = "${var.linux_base_ami}"
-  hostname_tag = "${var.hostname_tag}"
-  sec_grp_id = "${module.pacbot_sec_grp.security_group_id}"
-  ssh_key_name = "${var.ssh_key_name}"
-  vpc_subnet_id = "${var.vpc_subnet_id}"
-  vpc_id = "${var.vpc_id}"
-  ssh_source_ip = "${var.ssh_source_cidr_block}"
-  CostCenter = "${var.CostCenter}"
-  BusinessUnit = "${var.BusinessUnit}"
-  Environment = "${var.Environment}"
-  SysOwner = "${var.SysOwner}"
-  SysName  = "${var.SysName}"
-  Department = "${var.Department}"
-  tsm = "${var.tsm}"
-  Scheduler = "${var.Scheduler}"
-  PatchGroup = "${var.PatchGroup}"
-  instance_profile = "${module.Pacbot_ec2_role.ec2_instance_profile}"
 }
---
+++ Infrastructure/modules/ec2_profile/variables.tf
@@ -0,0 +1,3 @@
+variable "ec2_instance_profile_name" {}
+variable "ec2_iam_role_policy_name" {}
+variable "ec2_iam_role_name" {}
