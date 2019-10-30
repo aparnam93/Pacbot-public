7e26b634cccbe5e818abbc94590f7accbd920526
aparnam93 <priyanka.sharma@regeneron.com>
2019-10-30 15:01:21 +0530

    changing userdata

--- Infrastructure/userdata.sh
+++ Infrastructure/userdata.sh
@@ -6,15 +6,15 @@ yum install wget -y
 
 #Install and setup Python3.6
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install epel-release -y
-yum install python3-pip -y
-echo alias python3=python3.7 >> ~/.bashrc
-echo alias pip3=pip3.7 >> ~/.bashrc
-source ~/.bashrc
+sudo yum install epel-release -y
+sudo yum install python3-pip -y
+sudo echo alias python3=python3.7 >> ~/.bashrc
+sudo echo alias pip3=pip3.7 >> ~/.bashrc
+sudo source ~/.bashrc
 
 #Clone Repo
 
-git clone -b "development" https://ritscm.regeneron.com/scm/dcce/pacbot1.0.git
+git clone -b "development" https://github.com/aparnam93/Pacbot-public.git
 
 #Other Prereqs
 yum -y install java-1.8.0-openjdk docker maven unzip mysql
@@ -22,11 +22,11 @@ systemctl start docker
 wget https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip
 unzip terraform_0.11.8_linux_amd64.zip
 mv terraform /usr/bin/
-pip3.7 install -r /opt/pacbot/installer/requirements.txt
+pip3.7 install -r /opt/Pacbot-public/installer/requirements.txt
 
 
 #Setup UI components
-cd /opt/pacbot/webapp
+cd /opt/Pacbot-public/webapp
 sudo yum install nodejs npm -y
 npm install -g @angular/cli@1.6.8
 sudo npm install -g bower
@@ -37,10 +37,10 @@ bower install --allow-root
 
 
 #Copy the default Settings file and create a local.py
-cp /opt/pacbot/installer/settings/default.local.py  /opt/pacbot/installer/settings/local.py
+cp /opt/Pacbot-public/installer/settings/default.local.py  /opt/Pacbot-public/installer/settings/local.py
 
 
 # Launch UI
-cd /opt/pacbot/installer/
+cd /opt/Pacbot-public/installer/
 ng serve &>/dev/null
-python3.7 /opt/pacbot/installer/manager.py install
+python3.7 /opt/Pacbot-public/installer/manager.py install
