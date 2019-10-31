f549ba8526047ca6cf9d3766df3965ef33c9b472
aparnam93 <38241459+aparnam93@users.noreply.github.com>
2019-10-31 12:35:12 +0530

    Update InstanceState.sh

--- Infrastructure/InstanceState.sh
+++ Infrastructure/InstanceState.sh
@@ -3,7 +3,7 @@ if [[ $(aws ec2 describe-instances --instance-ids i-059c895c4b5d40a27 --query 'R
     fi
 instance_ip=$(cd /var/lib/jenkins/workspace/t-github-multibranch_development/Infrastructure && terraform output instance_ip)
 whoami
-ssh -tt -i /var/lib/jenkins/workspace/t-github-multibranch_development/Infrastructure/key.pem root@$instance_ip
+ssh -tt -i /var/lib/jenkins/workspace/t-github-multibranch_development/Infrastructure/key.pem ec2-user@$instance_ip
 
 sudo su 
 terraform --version
