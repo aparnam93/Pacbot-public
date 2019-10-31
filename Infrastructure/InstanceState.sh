f1112c1c0aea7de334d8d976395c2ebe6701a4b4
aparnam93 <priyanka.sharma@regeneron.com>
2019-10-31 12:22:37 +0530

    adding a shell script

--- Infrastructure/InstanceState.sh
+++ Infrastructure/InstanceState.sh
@@ -2,4 +2,4 @@ if [[ $(aws ec2 describe-instances --instance-ids i-059c895c4b5d40a27 --query 'R
     aws ec2 start-instances --instance-ids i-059c895c4b5d40a27 --region us-east-1; \
     fi
 instance_ip=$(cd /var/lib/jenkins/workspace/t-github-multibranch_development/Infrastructure && terraform output instance_ip)
-ssh -i /var/lib/jenkins/workspace/t-github-multibranch_development/Infrastructure/key.pem ec2-user@$instance_ip
+ssh -tt -i /var/lib/jenkins/workspace/t-github-multibranch_development/Infrastructure/key.pem ec2-user@$instance_ip
