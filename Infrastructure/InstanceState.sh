33427df565a61bb55c1ffb66ba04713ac0b16366
aparnam93 <priyanka.sharma@regeneron.com>
2019-10-31 11:22:25 +0530

    adding a shell script

---
+++ Infrastructure/InstanceState.sh
@@ -0,0 +1,3 @@
+if [[ $(aws ec2 describe-instances --instance-ids i-059c895c4b5d40a27 --query 'Reservations[].Instances[].State[].Name' --region us-east-1 --output text) = "stopped" ]] ; then \
+    aws ec2 start-instances --instance-ids i-059c895c4b5d40a27 ; \
+    fi
